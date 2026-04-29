b937ac87157f242955f6d8a941b8ad3597d28f1d
MiguelDZ123 <migui.manda1@gmail.com>
2026-04-28 21:54:06 -0400

    Added pricing page

--- app/components/home/Header.tsx
+++ app/components/home/Header.tsx
@@ -1,20 +1,15 @@
 import Link from 'next/link'
-import React from 'react'
 import SignIn from '../auth/googleSignIn'
 import { auth } from '@/app/auth/auth'
 import UserAvatar from '../auth/UserAvatar'
 import { Menu } from 'lucide-react'
-import { signIn } from "../../auth/auth"
 import {
     Popover,
     PopoverContent,
-    PopoverDescription,
     PopoverHeader,
-    PopoverTitle,
     PopoverTrigger,
 } from "@/components/ui/popover"
 import { SignOut } from '../auth/signOut'
-import { Sign } from 'crypto'
 
 
 const Header = async () => {
@@ -25,7 +20,9 @@ const Header = async () => {
     return (
         <div className='bg-[#111114] w-full mt-0 flex flex-row justify-between items-center gap-2 py-4 z-10 px-[15%] max-lg:px-10'>
             <div className='flex'>
-                <img src="/logobuilder.png" alt="LUNIO Builder Logo" className='h-10 w-full max-lg:h-6' />
+                <Link href="/">
+                    <img src="/logobuilder.png" alt="LUNIO Builder Logo" className='h-10 w-full max-lg:h-6' />
+                </Link>
             </div>
             <div className='flex items-center gap-6 justify-center max-md:hidden'>
                 <nav>
